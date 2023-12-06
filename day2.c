b733c155dc5c2230fa656f7c90863d880f003b9b
Samuel Neves <samuel.saraiva.neves@gmail.com>
2023-12-06 22:02:09 +0000

    Add files via upload

---
+++ day2.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+	int r = 0, g = 0, b = 0;
+	int i = 0;
+	int idGame = -1;
+	int sum = 0;
+	char buffer[200];
+	int num = 0;
+	int power = 1;
+	FILE* fp = NULL;
+
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+		return 1;
+	}
+	
+	fp = fopen(argv[1], "r");
+	if(fp == NULL) {
+		fprintf(stderr, "Error: File %s not found\n", argv[1]);
+		return 1;
+	}
+
+	while(feof(fp) ==	 0) {
+		r = 0; g = 0; b = 0;
+		fgets(buffer,200,fp);
+		if(buffer[0] != 'G') {
+			break;
+		}
+		sscanf(buffer, "Game %d:", &idGame);
+
+
+		for (i = 8; buffer[i] != '\n' ; i++) {
+			if ( buffer[i] >= 48 && buffer[i] <= 57) {
+				if (buffer[i+1] >= 48 && buffer[i+1] <= 57) {
+					num = (buffer[i] - 48) * 10 + (buffer[i+1]-48);
+					i++;
+					
+					if(buffer[i+2] == 'r') {
+						if(num > r) {
+							r = num;
+							continue;
+						}
+					}
+				
+					if(buffer[i+2] == 'g') {
+						if(num > g) {
+							g = num;
+							continue;
+						}
+					}
+
+					if(buffer[i+2] == 'b') {
+						if(num > b) {
+							b = num;
+							continue;
+						}
+					}
+				}
+				
+				if(buffer[i+2] == 'r') {
+					if(buffer[i] - 48 > r) {
+						r = buffer[i] - 48;
+						continue;
+					}
+				}
+				if(buffer[i+2] == 'g') {
+					if(buffer[i] - 48 > g) {
+						g = buffer[i] - 48;
+						continue;
+					}
+				}
+				if(buffer[i+2] == 'b') {
+					if(buffer[i] - 48 > b) {
+						b = buffer[i] - 48;
+						continue;
+					}
+				}
+			}
+		}
+
+		power = r * g * b;
+		buffer[0] = '\0';
+		sum += power;
+	}
+
+	printf("Soma: %d\n", sum);
+
+	return 0;
+}
